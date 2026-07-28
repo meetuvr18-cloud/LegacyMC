c1807df5fcd6872246e9628ad9c944345a45292b
meetuvr18-cloud <meetuvr18@gmail.com>
2026-07-29 03:33:03 +0530

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -93,8 +93,8 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#336699",
-      secondary: "#2F3136",
+      primary: "#a80a17",
+      secondary: "#d9c40b",
 
       // Standard status colors for success/error/warning/info messages.
       success: "#57F287",
